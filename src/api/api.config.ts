8662fefbba6d2a4c4228b8009075100d9ed71f51
Erikvilar <erikvilaralves@gmail.com>
2026-02-07 18:47:36 -0300

    configurando api

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,4 +1,3 @@
-
 export enum AppMode {
   LOCAL = "local",
   DEVELOPMENT = "development",
@@ -15,28 +14,33 @@ interface ApiConfig {
 }
 
 const localConfig: ApiConfig = {
-  BASE_URL: "http://192.168.100.105:8081/v1/agrion/",
-  URL: "192.168.100.105:8081/v1/agrion",
+  BASE_URL: "http://localhost:8090/v1/agrion/",
+  URL: "localhost:8090/v1/agrion",
   TIMEOUT_MS: 10000,
 };
 
 const devConfig: ApiConfig = {
-  BASE_URL: "http://141.148.158.42:25568/v1/agrion/",
-  URL: "141.148.158.42:25568/v1/agrion/",
+  BASE_URL: "http://76.13.236.54:8090/v1/agrion/",
+  URL: "76.13.236.54:8090/v1/agrion",
   TIMEOUT_MS: 10000,
 };
 
 const prodConfig: ApiConfig = {
-  BASE_URL: "https://back.stackpanel.com.br/v1/agrion/",
-  URL: "back.stackpanel.com.br/v1/agrion/",
+
+  BASE_URL: "https://webdataflux.cloud/v1/agrion/",
+  URL: "webdataflux.cloud/v1/agrion",
   TIMEOUT_MS: 8000,
 };
 
-const apiConfig: ApiConfig = currentMode === AppMode.PRODUCTION ? prodConfig : currentMode === AppMode.DEVELOPMENT ? devConfig : localConfig;
+const apiConfig: ApiConfig =
+    currentMode === AppMode.PRODUCTION
+        ? prodConfig
+        : currentMode === AppMode.DEVELOPMENT
+            ? devConfig
+            : localConfig;
 
 export const isLocal = currentMode === AppMode.LOCAL;
 export const isDev = currentMode === AppMode.DEVELOPMENT;
 export const isProd = currentMode === AppMode.PRODUCTION;
-
 
 export default apiConfig;
